da7db2573493dbdbd62395013b4ee93b6fb508b9
Mike McCallister <mike@mccllstr.com>
2014-03-24 01:53:35 -0500

    First commit with code that runs to completion.
    This version doesn't do anything useful, but it does run. Most of this
    code is based on the Cliff demo application. Certain techniques were
    also adopted from the OpenStack Python Client, particularly use of
    pbr.

---
+++ seadir/shell.py
@@ -0,0 +1,61 @@
+'''Main console script for the SEA Directory application.
+
+This script uses the Cliff framework. It it installed and configured
+through the package's setup.py file, so check the entry_points defined
+there.
+
+'''
+
+import logging
+import sys
+
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+import seadir
+
+
+class SeaDirApp(App):
+
+    CONSOLE_MESSAGE_FORMAT = '%(levelname)s: %(name)s %(message)s'
+
+    log = logging.getLogger(__name__)
+
+    def __init__(self):
+        super(SeaDirApp, self).__init__(
+            description=__doc__.strip(),
+            version=seadir.__version__,
+            command_manager=CommandManager('seadir.commands'),
+            )
+
+
+    def initialize_app(self, argv):
+        """ Application initialization code
+        """
+
+        # Pick up any default initializations
+        super(SeaDirApp, self).initialize_app(argv)
+
+        if self.options.debug:
+            self.dump_stack_trace = True
+        else:
+            self.dump_stack_trace = False
+
+        self.log.debug('initialize_app')
+
+
+    def prepare_to_run_command(self, cmd):
+        self.log.debug('prepare_to_run_command %s', cmd.__class__.__name__)
+
+
+    def clean_up(self, cmd, result, err):
+        self.log.debug('clean_up %s', cmd.__class__.__name__)
+        if err:
+            self.log.debug('got an error: %s', err)
+
+
+def main(argv=sys.argv[1:]):
+    return SeaDirApp().run(argv)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
